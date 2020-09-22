e79e3e3c53b1a097bc5620cae1c7a28cb9c46677
Skyler Tosh <skylerjtosh@gmail.com>
2020-09-22 02:09:26 -0700

    Weather information and city information are now updated in the html template.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    // update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Imperial.Value}</span>
+            <span>&deg;F</span>
+        </div>
+    `;
+
+};
 
 const updateCity = async (city) => {
    
    const cityDets = await getCity(city);
    const weather = await getWeather(cityDets.Key);
    
-   return {
-    cityDets: cityDets,
-    weather: weather
-   };
+   return { cityDets, weather };
 
 };
 
@@ -22,6 +38,6 @@ cityForm.addEventListener('submit', (e) => {
 
 	//update the ui with new city
     updateCity(city)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(err => console.log(err))
 });

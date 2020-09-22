22deddae3d2b48302b49eef58aaccad5466858c5
Skyler Tosh <skylerjtosh@gmail.com>
2020-09-21 23:03:29 -0700

    Created the getWeather function

--- js/forecast.js
+++ js/forecast.js
@@ -1,5 +1,19 @@
 const key = ''
 
+//get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query =  `${id}?apikey=${key}`;
+
+    const response = await fetch (base + query);
+    const data = await response.json();
+
+    return data[0];
+
+}
+
+// get city information
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -12,6 +26,8 @@ const getCity = async (city) => {
 
 };
 
-getCity('seattle')
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+getCity('seattle').then(data => {
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(data);
+}).catch(err => console.log(err));

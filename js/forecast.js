98041f9b09dd4a20820d585b4a8c29e1c0196dff
Skyler Tosh <skylerjtosh@gmail.com>
2020-09-21 22:44:12 -0700

    Created the rough function that handles grabbing the city api information

---
+++ js/forecast.js
@@ -0,0 +1,17 @@
+const key = ''
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+getCity('seattle')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
